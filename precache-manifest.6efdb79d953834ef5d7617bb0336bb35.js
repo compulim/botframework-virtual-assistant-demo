dc90add045e5e16da4491871c8f52ffd0a7d17ed
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-17 15:44:33 +0000

    Deploy compulim/botframework-virtual-assistant-demo to github.com/compulim/botframework-virtual-assistant-demo.git:gh-pages

---
+++ precache-manifest.6efdb79d953834ef5d7617bb0336bb35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7e767b5877b4204cc0dbd873f16f3b9",
+    "url": "/botframework-virtual-assistant-demo/static/media/CLO18_theGherkin_001.e7e767b5.jpg"
+  },
+  {
+    "revision": "e85d55f6e314fec5f32362ec56e3a9e4",
+    "url": "/botframework-virtual-assistant-demo/static/media/ArtboardElaineCroppedEmptied.e85d55f6.png"
+  },
+  {
+    "revision": "1e3e56e7b582e8f25215",
+    "url": "/botframework-virtual-assistant-demo/static/js/runtime~main.1e3e56e7.js"
+  },
+  {
+    "revision": "81d0d2c5dab562e95cdf",
+    "url": "/botframework-virtual-assistant-demo/static/js/main.81d0d2c5.chunk.js"
+  },
+  {
+    "revision": "5d21282855bae78768b8",
+    "url": "/botframework-virtual-assistant-demo/static/js/1.5d212828.chunk.js"
+  },
+  {
+    "revision": "81d0d2c5dab562e95cdf",
+    "url": "/botframework-virtual-assistant-demo/static/css/main.ed0412fb.chunk.css"
+  },
+  {
+    "revision": "9e7a83d1b282c24977241322eb5e45bf",
+    "url": "/botframework-virtual-assistant-demo/index.html"
+  }
+];
